4eada93fc5a58780c0b149141ab9bc88558fb0b2
jdfcc <3039898075@qq.com>
2022-11-09 21:30:23 +0800

    update

--- src/main/java/com/jdfcc/reggie/ReggieApplication.java
+++ src/main/java/com/jdfcc/reggie/ReggieApplication.java
@@ -1,17 +1,21 @@
 package com.jdfcc.reggie;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
 @EnableCaching//开启缓存注解
 @EnableTransactionManagement//开启事务
+@EnableKnife4j
+@EnableSwagger2
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class);
